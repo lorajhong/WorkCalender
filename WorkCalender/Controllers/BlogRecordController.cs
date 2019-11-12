9f0d2b9a8241b3577e5aeb2dcaf99b41a9221544
OU-YANG-JI-HAI <alex1101137204@gmail.com>
2019-11-12 23:24:33 +0800

    新增畫面  blog 紀錄、分析頁  view controller

---
+++ WorkCalender/Controllers/BlogRecordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkCalender.Controllers
+{
+    public class BlogRecordController : Controller
+    {
+        // GET: BlogRecord
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
