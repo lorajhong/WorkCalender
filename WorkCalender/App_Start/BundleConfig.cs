9d89963e41883c51fd382a3d4ffeaf7aa031157f
OU-YANG-JI-HAI <alex1101137204@gmail.com>
2019-11-12 23:16:01 +0800

    1. 引入行事曆 js css 2. 畫出基本 月、周、日 行事曆

--- WorkCalender/App_Start/BundleConfig.cs
+++ WorkCalender/App_Start/BundleConfig.cs
@@ -13,6 +13,21 @@ namespace WorkCalender
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            
+            //FullCalender JS
+            bundles.Add(new ScriptBundle("~/bundles/fullcalender")
+                .Include(
+                "~/Scripts/Fullcalender/core/main.min.js",
+                "~/Scripts/Fullcalender/core/locales-all.min.js",
+                "~/Scripts/Fullcalender/daygrid/main.min.js",
+                "~/Scripts/Fullcalender/google-calendar/main.min.js",
+                "~/Scripts/Fullcalender/interaction/main.min.js",
+                "~/Scripts/Fullcalender/list/main.min.js",
+                "~/Scripts/Fullcalender/luxon/main.min.js",
+                "~/Scripts/Fullcalender/moment/main.min.js",
+                "~/Scripts/Fullcalender/moment-timezone/main.min.js",
+                "~/Scripts/Fullcalender/rrule/main.min.js",
+                "~/Scripts/Fullcalender/timegrid/main.min.js"));
 
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
@@ -25,6 +40,14 @@ namespace WorkCalender
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //FullCalender Css
+            bundles.Add(new StyleBundle("~/Content/fullcalendercss")
+                .Include(
+                "~/Content/Fullcalender/core/main.min.css",
+                "~/Content/Fullcalender/daygrid/main.min.css",
+                "~/Content/Fullcalender/list/main.min.css",
+                "~/Content/Fullcalender/timegrid/main.min.css"));
         }
     }
 }
